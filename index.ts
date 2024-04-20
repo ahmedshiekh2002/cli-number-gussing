d2abd2201c617fb304facd6351ff725d4aa5aaab
unknown <champahmed2002@gmail.com>
2024-04-20 21:55:51 +0500

    number guessing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+
+
+const randomNumber = Math.floor(Math.random() * 10 + 1) ;
+const answer = await inquirer.prompt
+([
+    {
+       name:"userGuessNumber",
+       type:"number",
+       message:"Please Guess The Number between 1 till 10 = "
+    }
+])
+
+if(answer.userGuessNumber === randomNumber){
+    console.log("You guessed right number")
+}
+else{
+    console.log("You guessed wrong number")
+}
